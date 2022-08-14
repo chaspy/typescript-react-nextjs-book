a1669eaba7776f52e33ae80f91423e5f8e01d8a1
kondo takeshi <chaspy@users.noreply.github.com>
2022-08-14 15:47:38 +0900

    update

--- next-sample/pages/posts/[id].tsx
+++ next-sample/pages/posts/[id].tsx
@@ -1,6 +1,8 @@
+// 型を利用するためにインポート
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import Head from "next/head";
-import { useRouter } from "next/router";
+import { useRouter } from "next/router"; // next/routerからuseRouterというフックを取り込む
+import { ParsedUrlQuery } from "querystring";
 
 type PostProps = {
   id: string;
@@ -11,6 +13,7 @@ const Post: NextPage<PostProps> = (props) => {
   const router = useRouter();
 
   if (router.isFallback) {
+    // フォールバックページ向けの表示を返す
     return <div>Loading...</div>;
   }
 
@@ -21,14 +24,21 @@ const Post: NextPage<PostProps> = (props) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <p>このページは静的サイト生成によってビルド時に生成されたページです</p>
-        <p>{`/post/${id}に対応するページです`}</p>
+        {" "}
+        <p>
+          {" "}
+          このページは静的サイト生成によってビルド時に生成されたページです。
+        </p>
+        <p>{`/posts/${id}に対応するページです`}</p>
       </main>
     </div>
   );
 };
 
+// getStaticPathsは生成したいページのパスパラメータの組み合わせを返す
+// このファイルはpages/posts/[id].tsxなので、パスパラメータとしてidの値を返す必要がある
 export const getStaticPaths: GetStaticPaths = async () => {
+  // それぞれのページのパスパラメータをまとめたもの
   const paths = [
     {
       params: {
@@ -47,17 +57,22 @@ export const getStaticPaths: GetStaticPaths = async () => {
     },
   ];
 
+  // fallbackをfalseにすると、pathsで定義されたページ以外は404ページを表示する
   return { paths, fallback: false };
 };
 
-export const getStaticProps: GetStaticProps<PostProps> = async (context) => {
-  const id = Array.isArray(context.params["id"])
-    ? context.params["id"][0]
-    : context.params["id"];
+// パラメータの型を定義
+interface PostParams extends ParsedUrlQuery {
+  id: string;
+}
 
+// getStaticPaths実行後にそれぞれのパスに対してgetStaticPropsが実行される
+export const getStaticProps: GetStaticProps<PostProps, PostParams> = async (
+  context
+) => {
   return {
     props: {
-      id,
+      id: context.params!["id"],
     },
   };
 };
