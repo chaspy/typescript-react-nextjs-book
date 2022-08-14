0702cb78a81c1eec453b4c6613a561f8713fa1f1
kondo takeshi <chaspy@users.noreply.github.com>
2022-08-14 16:08:03 +0900

    fix

--- next-sample/pages/sayhello.tsx
+++ next-sample/pages/sayhello.tsx
@@ -1,16 +1,19 @@
 import { useState, useEffect } from "react";
 
-function Sayhello(){
-    const [data,setData] = useState({name:''})
+function Sayhello() {
+  // 内部で状態を持つためuseStateを利用
+  const [data, setData] = useState({ name: "" });
+  // 外部のAPIにリクエストするのは副作用なのでuseEffect内で処理
+  useEffect(() => {
+    // pages/api/hello.tsの内容にリクエスト
+    fetch("api/hello")
+      .then((res) => res.json())
+      .then((profile) => {
+        setData(profile);
+      });
+  }, []);
 
-useEffect(()=>{
-    fetch('api/hello')
-    .then((res => res.json())
-    .then((profile)=>{
-        setData(profile)
-    })
-},[])
-return <div>hello {data.name}</div>
+  return <div>hello {data.name}</div>;
 }
 
-export default Sayhello
+export default Sayhello;
